29d7d0183079e8d6bbd1e1e170426c2c641511c2
shimona <shimonawadhwa909@gmail.com>
2022-10-20 19:56:52 +0530

    power set algorithm

--- src/com/company/Cabcodechef.java
+++ src/com/company/Cabcodechef.java
@@ -18,38 +18,59 @@ public class Cabcodechef {
         Scanner cin = new Scanner(System.in);
         T = cin.nextInt();
         while (T-- > 0) {
+            int length=cin.nextInt();
+            String s1=cin.next();
+            String s2=cin.next();
+            String c="";
+            String result = "";
+            for (int i = 0; i < s1.length(); i++){
 
-            //ababbbab                     ab, ba
+                char ch = s1.charAt(i);
+                if (s2.indexOf(ch) != -1){
+                    c = c + ch;
+                    if (!result.contains("" + c.charAt(i)))
+                        result += "" + c.charAt(i);
+
+                }
+            }
+//            String result = "";
+//            for (int i = 0; i < c.length(); i++)
+//                if (!result.contains("" + c.charAt(i)))
+//                    result += "" + c.charAt(i);
+            int j=0;
+            int count1=0;
+            int count2=0;
+            int max=0;
+            for(j=0;j<result.length();j++) {
+                for (int i = 0; i < s1.length(); i++) {
+                    if (result.charAt(j) == s1.charAt(i)) {
+                        count1++;
+                    }
+                    if (result.charAt(j) == s2.charAt(i)) {
+                        count2++;
+                    }
+
+                }
+                if (count1 < count2) {
+                    if (count1 > max)
+                        max = count1;
+                } else {
+                    if (count2 <= count1) {
+                        if (count2 > max)
+                            max = count2;
+                    }
+                }
+                count1=0;
+                count2=0;
+            }
+            System.out.println(max);
+
+
+
         }
-    }
+   }
 
 
 }
-
-
-
-/*String S=cin.next();
-        String X=cin.next();
-        int a=Integer.parseInt(S);
-        int num=Integer.parseInt(X,2);
-        String b="";
-        for(int i=0;i<X.length();i++){
-            if(X.charAt(i)=='0')
-            {
-               b=b+'1';
-            }
-            else  {
-                b=b+'0';
-            }
-        }
-            int nums=Integer.parseInt(b,2);
-
-            int i=0;
-            for(i=0;i<a;i++){
-                if(num/((int)Math.pow(2,i))<=nums)
-                    break;
-            }
-            System.out.println(i);*/
-
 
 
